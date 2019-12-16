dc39207809682b05746afc0c82d0cf467b2aea1d
Marc Almond Valleza <majvalleza@gmail.com>
2019-12-16 17:07:16 +0800

    chore: make description nullable

--- database/migrations/2019_12_15_190458_add_description_to_users.php
+++ database/migrations/2019_12_15_190458_add_description_to_users.php
@@ -14,7 +14,7 @@ class AddDescriptionToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->longText('description');
+            $table->longText('description')->nullable();
         });
     }
 
